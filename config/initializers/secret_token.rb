72b0afc80bfefdc0e3e8a92cc2bf9db1f08b7d41
Masato IKEDA <ikeda_m@newness.jp>
2010-09-29 16:53:36 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolymorphicAssociationSample::Application.config.secret_token = 'be7d979b89a8cf26864df9f00107f8eb18ed834bf1fc0fb2881c54b55fc9769d4b688cccca371b01f27d958e582e495d0f3e235e4e5c027be37361df480b6631'
